263e9d54b7d26ce59af08787eda761985e0b5b05
Seth- <lovengiro@gmail.com>
2026-01-26 23:29:25 +0200

    Remove static export, use Vercel SSR

--- next.config.js
+++ next.config.js
@@ -1,19 +1,8 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require("./next-i18next.config");
 
-// module.exports = {
-//   reactStrictMode: true,
-//   // any other config
-// };
-
 // next.config.js
 module.exports = {
   reactStrictMode: false,
   i18n,
 };
-
-// module.exports = {
-//   images: {
-//     domains: ['your-supabase-bucket-url.supabase.co', 'another-domain.com'],
-//   },
-// };
